640cd9f489db61ce0f938e6246c7af77a796775f
Akash <akash123@gmail.com>
2026-04-10 08:57:35 +0530

    updated code

--- code.tf
+++ code.tf
@@ -13,12 +13,12 @@ provider azurerm {
     }
 }
 
-resource "azurerm_resource_group" "moon"{
-    name = "merarghai"
+resource "azurerm_resource_group" "moon1"{
+    name = "merarghaibhai"
     location = "west us"
 }
 resource "azurerm_storage_account" "meramoon"{
-    name = "merastoragehai97111"
+    name = "merastoragehai9711172170"
     resource_group_name = "merarghai"
     location = "west us"
     account_tier = "Standard"
