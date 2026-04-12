bb638e9ac26b23bbce6940dde2b1bc2ab60a0a6d
Akash <akash123@gmail.com>
2026-04-12 19:22:20 +0530

    updated

--- code.tf
+++ code.tf
@@ -1,31 +1,39 @@
 terraform {
-    required_providers {
-        azurerm = {
-            source = "hashicorp/azurerm"
-            version = "4.67.0"
-        }
+  backend "azurerm" {
+    resource_group_name = "akrg"
+    storage_account_name = "akstore0505"
+    container_name = "myconetf"
+    key  = "terraform.tfstate"
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.67.0"
     }
+  }
+}
+provider "azurerm" {
+  features {
+  }
 }
 
-provider azurerm {
-    features {
+resource "azurerm_resource_group" "akrg" {
+  name     = "akrg"
+  location = "west us"
+}
+resource "azurerm_storage_account" "akstore" {
 
-    }
+
+  name                     = "akstore0505"
+  resource_group_name      = "akrg"
+  location                 = "west us"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
 
-resource "azurerm_resource_group" "moon1"{
-    name = "merarghaibhai"
-    location = "west us"
-}
-resource "azurerm_storage_account" "meramoon"{
-    name = "merastoragehai9711172170"
-    resource_group_name = "merarghai"
-    location = "central india"
-    account_tier = "Standard"
-    account_replication_type = "LRS"
-<<<<<<< HEAD
-}
-hello from both
-=======
+resource "azurerm_storage_container" "mycon" {
+  name                 = "myconetf"
+  storage_account_name = "akstore0505"
+  container_access_type  = "private"
 
->>>>>>> second
+}
